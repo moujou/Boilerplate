cb4c53ad3f1af83c133ab2a20aae7e07038d1486
Patrick Domscheid <patrick.domscheid@googlemail.com>
2016-05-04 16:31:41 +0200

    buttonFunction

--- CNC/Interface/design/ButtonFunction.js
+++ CNC/Interface/design/ButtonFunction.js
@@ -2,8 +2,11 @@ var toggleButton = function(id){
 	
     if (document.getElementById(id).innerHTML == "Start") {
 		document.getElementById(id).innerHTML = "Stop";
+		sendActionInfo(id,"false");
+
     } else {
         document.getElementById(id).innerHTML = "Start";
+        sendActionInfo(id,"true");
     }
 };
 
@@ -13,4 +16,19 @@ var refreshButtonTask = function() {
 
 var refreshButtonStatus = function() {
 	initialize();
+}
+
+var sendActionInfo(id, cmd) {
+
+	var xhr    = new XMLHttpRequest();
+	var content = document.querySelector('#status-overview tbody');
+	
+
+	xhr.open('POST', 'http://botnet.artificial.engineering:8080/api/Status');
+
+	xhr.responseType = 'json';
+
+	data = {'id': id, 'status': cmd};
+
+	xhr.send(JSON.stringify(data));
 }
