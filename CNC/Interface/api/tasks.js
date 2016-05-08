a45c792c83657646fdee07b0b449d80ca1633283
moujou <mou.mack@yahoo.de>
2016-05-08 17:32:20 +0200

    Kleine Änderungen vorgenommen und paar Sachen die unnötig waren entfernt

--- CNC/Interface/api/tasks.js
+++ CNC/Interface/api/tasks.js
@@ -38,3 +38,21 @@ var taskRequest = function() {
 	xhr.send(null);
 
 };
+
+var sendApiTask = function(id, type, data) {
+	
+	var xhr = new XMLHttpRequest();
+	
+	xhr.open('POST', 'http://botnet.artificial.engineering:8080/api/Tasks');
+
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.setRequestHeader('Token', 'cc444569854e9de0b084ab2b8b1532b2');
+	xhr.responseType = 'json';
+	
+	data = {"id" : parseInt(id,10), 
+			"type" : type, 
+			"data" : {'input': data,
+					'output' : null} 
+	};
+	xhr.send(JSON.stringify(data));
+};
