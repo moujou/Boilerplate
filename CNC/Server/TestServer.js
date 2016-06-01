662ee0ec645e2724410e6910472cfa6c82bdec72
moujou <mou.mack@yahoo.de>
2016-06-01 18:18:16 +0200

    an dem server etwas gearbeitet

--- CNC/Server/TestServer.js
+++ CNC/Server/TestServer.js
@@ -10,6 +10,12 @@ app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
 var datenbankStatus =  [];
+
+app.post('api/Status', (req, res) => {
+	var adminToken = 'cc444569854e9de0b084ab2b8b1532b2';
+	var user 	   = req.get('Token');
+	
+});
 	
 fs.readFile('./status.txt','utf8', (err, data) => {
 	if (err) throw err;
@@ -25,8 +31,7 @@ app.get('/api/Status', (req, res) => {
 
 var datenbankTasks = [];
 
-fs.readFile('./tasks.txt','utf8', (err, data) => {
-	if (err) throw err;
+fs.readFile('./task.txt','utf8', (err, data) => {
 	
 	datenbankTasks = JSON.parse(data.toString());
 });
@@ -36,6 +41,23 @@ app.get('/api/Tasks', (req, res) => {
 	if(datenbankTasks instanceof Array) {
 		res.send(JSON.stringify(datenbankTasks));
 	}
+});
+
+app.post('/api/Tasks', (req, res) => {
+	var admin	   = 'cc444569854e9de0b084ab2b8b1532b2';
+	var user 	   = req.get('Token');
+	
+	if(req.body !== null) {
+		datenbankTasks.push(req.body);
+	} else {
+		console.log("Fail - Task")
+	}
+	
+	fs.writeFile('./task.txt', JSON.stringify(req.body), (err) => {
+		if(err) throw err;
+		
+		console.log("Task geschrieben!");
+	});
 });
 
 app.listen(3000);
